561ba75bd038dae255c4867dc0e774fc5a241be0
Jiheon-Lee <wlgjsdl0@gmail.com>
2021-08-09 17:22:15 +0900

    Add Node Server API

--- Node.js/fastcampus-node/src/main.js
+++ Node.js/fastcampus-node/src/main.js
@@ -1,162 +1,77 @@
 // @ts-check
-/* eslint-disable no-restricted-syntax */
+
+const http = require('http')
+const { title } = require('process')
 
 /**
- * @typeof Person
- *
- * @property {number} age
- * @property {string} city
- * @property {string | string[]} [pet]
- *
- * */
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
 
-/** @type {Person[]} */
-const people = [
+/** @type {Post[]} */
+const posts = [
   {
-    age: 20,
-    city: '서울',
-    pet: ['cat', 'dog'],
+    id: 'my_first_post',
+    title: 'My first post',
+    content: 'Hello',
   },
   {
-    age: 40,
-    city: '부산',
-  },
-  {
-    age: 31,
-    city: '대구',
-    pet: ['cat', 'dog'],
-  },
-  {
-    age: 36,
-    city: '서울',
-  },
-  {
-    age: 27,
-    city: '부산',
-    pet: 'cat',
-  },
-  {
-    age: 24,
-    city: '서울',
-    pet: 'dog',
+    id: 'my_second_post',
+    title: '나의 두번째 포스트',
+    content: 'Second post!',
   },
 ]
 
-/**
- * A. 30대 미만이 한 명이라도 사는 모든 도시
- * B. 각 도시별로 개와 고양이를 키우는 사람의 수
- */
+const server = http.createServer((req, res) => {
+  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
+  const postIdRegexResult =
+    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
 
-function solveA() {
-  /** @type {string[]} */
-  const cities = []
-
-  for (const person of people) {
-    if (person.age < 30) {
-      if (!cities.find((city) => person.city === city)) {
-        cities.push(person.city)
-      }
+  if (req.url === '/posts' && req.method === 'GET') {
+    // GET /posts
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalCount: posts.length,
     }
-  }
 
-  return cities
-}
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.end(JSON.stringify(result))
+  } else if (postIdRegexResult && req.method === 'GET') {
+    // GET /posts:id
+    const postId = postIdRegexResult[1]
+    const post = posts.find((_post) => _post.id === postId)
 
-function solveAModern() {
-  const allCities = people
-    // .filter((person) => person.age < 30)
-    .filter(({ age }) => age < 30)
-    // .map((person) => person.city)
-    .map(({ city }) => city)
-  const set = new Set(allCities)
-  return Array.from(set)
-}
-
-// console.log('solveA', solveA())
-// console.log('solveAModern', solveAModern())
-
-// B. 각 도시별로 개와 고양이를 키우는 사람의 수
-
-/**
- * {
- *    "서울": {
- *      "dog": 2,
- *      "cat": 1,
- *    },
- *    "대구": {
- *      "dog": 1,
- *      "cat": 1,
- *    },
- *    "부산": {
- *      "cat": 1,
- *    },
- * }
- */
-
-/** @typedef {Object.<string, Object.<string, number>>} PetsOfCities */
-
-function solveB() {
-  /** @type {PetsOfCities} */
-  const result = {}
-
-  for (const person of people) {
-    const { city, pet: petOrPets } = person
-
-    if (petOrPets) {
-      const petsOfCity = result[city] || {}
-
-      if (typeof petOrPets === 'string') {
-        const pet = petOrPets
-        const origNumPetsOfCity = petsOfCity[pet] || 0
-        petsOfCity[pet] = origNumPetsOfCity + 1
-      } else {
-        for (const pet of petOrPets) {
-          const origNumPetsOfCity = petsOfCity[pet] || 0
-          petsOfCity[pet] = origNumPetsOfCity + 1
-        }
-      }
-
-      result[city] = petsOfCity
+    if (post) {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.end(JSON.stringify(post))
+    } else {
+      res.statusCode = 404
+      res.end('Post not found.')
     }
-  }
-
-  return result
-}
-
-/**
- * [
- *   ["서울", "cat"],
- *   ["서울", "dog"],
- *   ["부산", "dog"],
- * ]
- */
-
-function solveBModern() {
-  return people
-    .map(({ pet: petOrPets, city }) => {
-      const pets =
-        (typeof petOrPets === 'string' ? [petOrPets] : petOrPets) || []
-
-      return {
-        city,
-        pets,
-      }
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    // POST /posts
+    req.setEncoding('utf-8')
+    req.on('data', (data) => {
+      console.log(data)
     })
-    .flatMap(({ city, pets }) => pets.map((pet) => [city, pet]))
-    .reduce((/** @type {PetsOfCities} */ result, [city, pet]) => {
-      if (!city || !pet) {
-        return result
-      }
 
-      return {
-        ...result,
-        [city]: {
-          ...result[city],
-          [pet]: (result[city]?.[pet] || 0) + 1,
-        },
-      }
-    }, {})
-}
+    res.statusCode = 200
+    res.end('Creating post')
+  } else {
+    res.statusCode = 404
+    res.end('Not found.')
+  }
+})
 
-console.log('solveB', solveB())
-console.log('solveBModern', solveBModern())
+const PORT = 3000
+
+server.listen(PORT, () => {
+  console.log(`The server is listening at port: ${PORT}`)
+})
