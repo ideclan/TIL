6c74e307f9ec22628f0f5c4321270d915df975bb
Jiheon-Lee <wlgjsdl0@gmail.com>
2021-08-21 17:50:30 +0900

    Add init files

--- Node.js/fastcampus-node/src/main.js
+++ Node.js/fastcampus-node/src/main.js
@@ -1,13 +1,24 @@
 // @ts-check
 
+/* eslint-disable no-console */
+
 const express = require('express')
 
 const app = express()
+app.use(express.json())
+app.set('views', 'src/views')
+app.set('view engine', 'pug')
 
 const PORT = 3000
 
-app.use('/', (req, res) => {
-  res.send('Hello, express!')
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+app.use('/public', express.static('src/public'))
+
+app.use((err, req, res, next) => {
+  res.statusCode = err.statusCode || 500
+  res.send(err.message)
 })
 
 app.listen(PORT, () => {
---
+++ Node.js/fastcampus-node/src/routes/users.js
@@ -0,0 +1,60 @@
+const express = require('express')
+
+const router = express.Router()
+
+const USERS = {
+  15: {
+    nickname: 'foo',
+  },
+  16: {
+    nickname: 'bar',
+  },
+}
+
+router.get('/', (req, res) => {
+  res.send('User list')
+})
+
+router.param('id', (req, res, next, value) => {
+  // @ts-ignore
+  const user = USERS[value]
+
+  if (!user) {
+    const err = new Error('User not found.')
+    err.statusCode = 404
+    throw err
+  }
+
+  // @ts-ignore
+  req.user = USERS[value]
+  next()
+})
+
+router.get('/:id', (req, res) => {
+  const resMimeType = req.accepts(['json', 'html'])
+
+  if (resMimeType === 'json') {
+    // @ts-ignore
+    res.send(req.user)
+  } else if (resMimeType === 'html') {
+    res.render('user-profile', {
+      nickname: req.user.nickname,
+    })
+  }
+})
+
+router.post('/', (req, res) => {
+  res.send('User registered.')
+})
+
+router.post('/:id/nickname', (req, res) => {
+  // @ts-ignore
+  const { user } = req
+  const { nickname } = req.body
+
+  user.nickname = nickname
+
+  res.send(`User nickname updated: ${nickname}`)
+})
+
+module.exports = router
